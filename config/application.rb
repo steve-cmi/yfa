64670239e517f14b750093e1ff914f1ca3a3ccd4
Steve Friedman <steve@commonmedia.com>
2014-11-14 17:08:41 -0500

    Update and move config.assets.precompile. Should be defined for all even if there's no precompile in dev.

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,7 @@ module Yfa
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w( bootstrap.css.map bootstrap-theme.css.map )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,7 @@ Yfa::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( admin.js auditions.js search.js films.js flickrshow-7.2.js html5shiv-p.js )
+  # config.assets.precompile += %w( admin.js auditions.js search.js films.js flickrshow-7.2.js html5shiv-p.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
