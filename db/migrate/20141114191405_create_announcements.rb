b2816a3e55593a102c93c6fe03afdcee2064505c
Steve Friedman <steve@commonmedia.com>
2014-11-14 17:13:15 -0500

    Create announcement migration and model.

---
+++ db/migrate/20141114191405_create_announcements.rb
@@ -0,0 +1,11 @@
+class CreateAnnouncements < ActiveRecord::Migration
+  def change
+    create_table :announcements do |t|
+      t.string :title
+      t.string :body
+      t.string :link_text
+      t.string :link_url
+      t.timestamps
+    end
+  end
+end
