44a5df0ba6d1f0759f7c6a45a54c2efbc0b48484
Steve Friedman <steve@commonmedia.com>
2014-11-19 15:59:38 -0500

    Validate uniqueness of filters.

--- app/models/events_filter.rb
+++ app/models/events_filter.rb
@@ -3,4 +3,6 @@ class EventsFilter < ActiveRecord::Base
   belongs_to :event
   belongs_to :filter
 
+  validates :filter_id, :uniqueness => {:scope => :event_id, :message => "has already been assigned to this event"}
+
 end
