1b697a6dac39a82d91aa54880a556e6ea855e723
Steve Friedman <steve@commonmedia.com>
2014-12-02 15:08:33 -0500

    Issue with reversible update_films is not enum, it's that remove_column can't be reversed in Rails 3.

--- db/migrate/20141114150953_update_films.rb
+++ db/migrate/20141114150953_update_films.rb
@@ -1,14 +1,28 @@
 class UpdateFilms < ActiveRecord::Migration
-  def change
-    remove_column :films, :writer, :string
-    remove_column :films, :picture_meta, :string
-    remove_column :films, :flickr_id, :string
-    remove_column :films, :location, :string
-    remove_column :films, :aud_files, :text
-    remove_column :films, :public_aud_info, :boolean
-    remove_column :films, :url_key, :string
-    remove_column :film_positions, :assistant, :string
-    remove_column :showtimes, :email_sent, :boolean
+  def up
+    remove_column :films, :writer
+    remove_column :films, :picture_meta
+    remove_column :films, :flickr_id
+    remove_column :films, :location
+    remove_column :films, :aud_files
+    remove_column :films, :public_aud_info
+    remove_column :films, :url_key
+    remove_column :film_positions, :assistant
+    remove_column :showtimes, :email_sent
+    rename_column :showtimes, :timestamp, :starts_at
+    rename_column :auditions, :timestamp, :starts_at
+  end
+
+  def down
+    add_column :films, :writer, :string
+    add_column :films, :picture_meta, :string
+    add_column :films, :flickr_id, :string
+    add_column :films, :location, :string
+    add_column :films, :aud_files, :text
+    add_column :films, :public_aud_info, :boolean
+    add_column :films, :url_key, :string
+    add_column :film_positions, :assistant, :enum, :limit => %w(Associate Assistant)
+    add_column :showtimes, :email_sent, :boolean
     rename_column :showtimes, :timestamp, :starts_at
     rename_column :auditions, :timestamp, :starts_at
   end
