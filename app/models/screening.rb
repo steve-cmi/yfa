bad5080582cdf046224f706f9a9cb9b717d31b49
Steve Friedman <steve@commonmedia.com>
2014-12-09 09:50:23 -0500

    Remove obsolete screening mailer.

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -2,16 +2,6 @@ class Screening < ActiveRecord::Base
 	belongs_to :film
   belongs_to :building
 
-	# Does YFA want this?
-	# after_create :notify_oup
-	# after_update :notify_oup
-
-	# def notify_oup
-	# 	return unless self.starts_at_changed?
-	# 	@film = self.film rescue nil # will error if film can't be found, meaning not approved
-	# 	ScreeningMailer.notify_oup_email(@film,self).deliver if @film && @film.approved		
-	# end
-
 	def self.by_date
 		order(:starts_at)
 	end
