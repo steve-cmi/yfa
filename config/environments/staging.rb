cc242280df2ba248c49d2b9a7bf1b92b5b3e2a9b
Steve Friedman <steve@commonmedia.com>
2014-12-17 16:41:35 -0500

    Add sendgrid settings to heroku staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,8 +78,20 @@ Yfa::Application.configure do
 
   # Root url used for action mailer links
   config.action_mailer.default_url_options = { :host => "yfa-staging.herokuapp.com" }
-  
-  #Use notifier plugin gem
+
+  # Sendgrid
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+  # Use notifier plugin gem
   # config.middleware.use ExceptionNotification::Rack,
   # 	:email => { 
   #     :email_prefix => "[YFA Site] ",
