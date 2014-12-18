e1db5c786e77ff76cc4fc840d467bedae6f202ad
Steve Friedman <steve@commonmedia.com>
2014-12-18 10:43:39 -0500

    Remove ftp.yml and its initializer. Doesn't seem to be used!

--- config/initializers/ftp.rb
+++
@@ -1,9 +0,0 @@
-file_name = Rails.root.join('config/ftp.yml')
-
-if File.exists?(file_name)
-  ftp_settings = YAML.load_file(file_name)[Rails.env]
-
-  FTP_HOST = ftp_settings["host"]
-  FTP_USERNAME = ftp_settings["user"]
-  FTP_PASSWORD = ftp_settings["password"]
-end
