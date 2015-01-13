1831c43e8cae7586f0d988347741f016311a4d01
Steve Friedman <steve@commonmedia.com>
2015-01-13 10:51:50 -0500

    Preventing enter key submissions in admin.js should not include search bar.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	// There will be no enter key submissions here
-	$("form").bind("keypress", function(e) {
+	$("form").not('.navbar-form').bind("keypress", function(e) {
     if (e.keyCode == 13) return false;
   });
   
